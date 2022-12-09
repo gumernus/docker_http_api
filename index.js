f2d4dacd965c523e0428870068688ebb5935434c
gumernus <gumernusbussines@gmail.com>
2022-12-09 20:33:23 +0100

    uh

--- index.js
+++ index.js
@@ -2,59 +2,43 @@ import http from "http"
 
 export class Docker_API {
 
-				constructor(SOCKET_PATH) {
-								// do some error handlind
-								this.SOCKET_PATH = SOCKET_PATH;
-				}
+	constructor(SOCKET_PATH) {
+		// do some error handlind
+		this.SOCKET_PATH = SOCKET_PATH;
+	}
 
-				listContainers() {
-								let options = {
-												socketPath: this.SOCKET_PATH,
-												path: "http://localhost/v1.41/containers/json"
-								}
-
-								let clientRequest = http.request(options, res => {
-												res.setEncoding("utf8");
-												res.on('data', data => console.log(JSON.parse(data)));
-												res.on("error", error => console.error(error));
-	      });
-
-								clientRequest.end();
-				}
-
-				createContainer(imageName) {
-								let options = {
-												socketPath: this.SOCKET_PATH,
-												path: "http://localhost/v1.41/containers/create",
-												method: "POST",
-												headers: {"Content-Type": "application/json"}
-								}
-
-								let clientRequest = http.request(options, res => {
-												res.setEncoding("utf8");
-												res.on("data", data => console.log(JSON.parse(data)));
-												res.on("error", error => console.error(error));
-								});
-
-								let payload = JSON.stringify({ "Image": imageName })
-
-								clientRequest.write(payload);
-								clientRequest.end();
+	listContainers() {
+		let options = {
+			socketPath: this.SOCKET_PATH,
+			path: "http://localhost/v1.41/containers/json"
 		}
 
-		test() {
-				let options = {
-						socketPath: this.SOCKET_PATH,
-						path: "http://localhost/v1.41/containers/json"
-	      }
+		let clientRequest = http.request(options, res => {
+			res.setEncoding("utf8");
+			res.on('data', data => console.log(JSON.parse(data)));
+			res.on("error", error => console.error(error));
+		});
 
-				let clientRequest = http.request(options, res => {
-						res.setEncoding("utf8");
-				    res.on('data', data => console.log(JSON.parse(data)));
-						res.on("error", error => console.error(error));
-	      });
+		clientRequest.end();
+	}
 
-		    clientRequest.end();
-		
 
-}
+	createContainer(imageName) {
+		let options = {
+			socketPath: this.SOCKET_PATH,
+			path: "http://localhost/v1.41/containers/create",
+			method: "POST",
+			headers: {"Content-Type": "application/json"}
+		}
+
+		let clientRequest = http.request(options, res => {
+			res.setEncoding("utf8");
+			res.on("data", data => console.log(JSON.parse(data)));
+			res.on("error", error => console.error(error));
+		});
+
+		let payload = JSON.stringify({ "Image": imageName })
+
+		lientRequest.write(payload);
+		clientRequest.end();
+	}
