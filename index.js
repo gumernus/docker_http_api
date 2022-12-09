d2907ecad22a51573817a7d6635b9c9d9d6cb8d5
gumernus <gumernusbussines@gmail.com>
2022-12-09 16:41:20 +0100

    fixed styling

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
 import http from "http"
 
-// const SOCKET_PATH = "/var/run/docker.sock"
-
 export class Docker_API {
 
-	constructor(SOCKET_PATH) {
-		// do some error handlind
-		this.SOCKET_PATH = SOCKET_PATH;
-	}
+		constructor(SOCKET_PATH) {
+				// do some error handlind
+				this.SOCKET_PATH = SOCKET_PATH;
+		}
 
-	listContainers() {
-		    let options = {
-			        socketPath: this.SOCKET_PATH,
-				    path: "http://localhost/v1.41/containers/json"
-	        }
-		    let clientRequest = http.request(options, res => {
-			        res.setEncoding("utf8");
+		listContainers() {
+				let options = {
+						socketPath: this.SOCKET_PATH,
+						path: "http://localhost/v1.41/containers/json"
+	      }
+				let clientRequest = http.request(options, res => {
+						res.setEncoding("utf8");
 				    res.on("data", data => console.log(JSON.parse(data)));
-					res.on("error", error => console.error(error));
-	        });
+						res.on("error", error => console.error(error));
+	      });
 		    clientRequest.end();
-	}
+		}
 
-	createContainer() {
-		    let options = {
-			        socketPath: this.SOCKET_PATH,
+		createContainer() {
+				let options = {
+						socketPath: this.SOCKET_PATH,
 				    path: "http://localhost/v1.41/containers/create",
-					method: "POST",
-	                headers: {"Content-Type": "application/json"}
-		    }
-	        let payload = JSON.stringify({ "Image": "alpine" })
+						method: "POST",
+	          headers: {"Content-Type": "application/json"}
+				}
+	      let payload = JSON.stringify({ "Image": "alpine" })
 		    let clientRequest = http.request(options, res => {
-			        res.setEncoding("utf8");
+			      res.setEncoding("utf8");
 				    res.on("data", data => console.log(JSON.parse(data)));
-					res.on("error", error => console.error(error));
-	        });
+						res.on("error", error => console.error(error));
+	      });
 		    clientRequest.write(payload);
-			clientRequest.end();
-	}
+				clientRequest.end();
+		}
 
 }
