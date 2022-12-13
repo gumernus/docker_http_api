2b27474f6d32a8c036674e9f61033ae0579ed059
gumernus <gumernusbussines@gmail.com>
2022-12-13 18:12:06 +0100

    some explaining for the parameters

--- examples/create_container.js
+++ examples/create_container.js
@@ -3,3 +3,6 @@ import { Docker } from "../src/index.js"
 let docker = new Docker();
 
 console.log(await docker.createContainer("alpine", "test1"));
+
+// parameter 1: name of image
+// parameter 2: name of container
