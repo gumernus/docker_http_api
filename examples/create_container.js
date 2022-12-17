2d229792539e862b3e253f058ccf6eed51a7e548
gumernus <gumernusbussines@gmail.com>
2022-12-17 21:13:44 +0100

    big changes for createContainer()

--- examples/create_container.js
+++ examples/create_container.js
@@ -2,6 +2,15 @@ import { Docker } from "../src/index.js"
 
 let docker = new Docker();
 
-console.log(await docker.createContainer("<image name>", "<name for container>"));
+console.log(await docker.createContainer({
+	start: true, // start the container after creating
+	image: "alpine", // image name
+	entrypoint: "/bin/sh", // enetrypoint 
+	ports: [ // allow ports
+		{host: "80", container: "8080"}, 
+		{host: "1234", container: "4321"}
+	]
+}));
 
-// will create and start container
+// you can also add: 
+// - memory: 
--- src/index.js
+++ src/index.js
@@ -1,57 +1,69 @@
 import papi from "papi"
 
+function customErr(text) {
+	console.log(new Error(text))
+}
+
 export class Docker extends papi.Client {
 
 	constructor(opts) {
 		opts = opts || {};
 	    opts.baseUrl = "http://localhost/v1.41";
+		opts.socketPath = opts.socketPath ? opts.socketPath : "/var/run/docker.sock"
 		super(opts);
   	}
 
 	async listContainers() {
-		let opts = {
-      		path: `/containers/json`,
-			socketPath: "/var/run/docker.sock"
-    	}
+		let opts = { path: `/containers/json` }
 		let res = await this._get(opts);
 	    return res.body;
   	}
 
-	async createContainer(image, name) {
+	async createContainer(options) {
+		if(!options.image) { options.image = customErr(`Image name for the new container was not included.`) }
+		if(!options.name) { options.name = Math.random().toString(36).slice(2, 7) }
+		if(!options.entrypoint) { options.entrypoint = "/bin/sh" }
+
+		if(!options.memory) { options.memory = 0 }
+		if(!options.memoryReservation) { options.memoryReservation = 0 }
+
+		let ports = {};
+		if(options.ports && typeof options.ports == "object") { 
+			for(let port of options.ports) {
+				if(typeof port.host != "string") { port.host.toString() }
+				if(typeof port.container != "string") { port.container.toString() }
+				ports[`${port.host}/tcp`] = [{"HostPort": port.container}]		
+			}
+		}
+
 		let opts = {
-			path: `/containers/create?name=${name}`,
+			path: `/containers/create?name=${options.name}`,
 			headers: {"Content-Type": "application/json"}, 
 			body: {
-				"Image": image,
-				"Hostname": name,
+				"Image": options.image,
+				"Hostname": options.name,
+				"Entrypoint": options.entrypoint,
 				"HostConfig":{
-					"PortBindings":{
-						"80/tcp":[{"HostPort":"80"}]
-					}
+					"Memory": options.memory,
+					"MemoryReservation": options.memoryReservation,
+					"PortBindings": ports
 				},
 				"Tty": true
 			},
-			socketPath: "/var/run/docker.sock"
 		}
 		let res = await this._post(opts);
-		this.startContainer(res.body.Id)
+		if(options.start && options.start == true) { this.startContainer(res.body.Id) }
 		return res.body;
 	}
 
 	async startContainer(id) {
-		let opts = {
-			path: `/containers/${id}/start`,
-			socketPath: "/var/run/docker.sock",
-		}
+		let opts = { path: `/containers/${id}/start` }
 		let res = await this._post(opts);
 		return res.body;
 	}
 
 	async clearContainers() { // don't use in prod
-		let opts = {
-			path: `/containers/prune`,
-			socketPath: "/var/run/docker.sock",
-		}
+		let opts = { path: `/containers/prune` }
 		let res = await this._post(opts);
 		return res.body;
 	}
