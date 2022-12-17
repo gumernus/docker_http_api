3ab395c1b74d8f77ac161662b393ac9c203830ba
gumernus <gumernusbussines@gmail.com>
2022-12-17 21:43:32 +0100

    update example

--- examples/create_container.js
+++ examples/create_container.js
@@ -5,6 +5,7 @@ let docker = new Docker();
 console.log(await docker.createContainer({
 	start: true, // start the container after creating
 	image: "alpine", // image name
+	name: "my_lovely_container", // name for the container
 	entrypoint: "/bin/sh", // enetrypoint 
 	ports: [ // allow ports
 		{host: "80", container: "8080"}, 
@@ -13,4 +14,5 @@ console.log(await docker.createContainer({
 }));
 
 // you can also add: 
-// - memory: 
+// - memory: 1000 (in bytes)
+// - memoryReservation (in bytes)
