b14ea68b8a5cd27dac6d7126debcb40b1b33c331
gumernus <gumernusbussines@gmail.com>
2022-12-23 11:07:33 +0100

    deleted examples

--- examples/clear_containers.js
+++
@@ -1,7 +0,0 @@
-import { Docker } from "../src/index.js"
-
-let docker = new Docker();
-
-console.log(await docker.clearContainers());
-
-// this will delete all stopped containers
--- examples/list_containers.js
+++
@@ -1,5 +0,0 @@
-import { Docker } from "../src/index.js"
-
-let docker = new Docker();
-
-console.log(await docker.listContainers());
